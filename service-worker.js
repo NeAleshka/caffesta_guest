006db9f8a6cdf29d6069292ebaaf6d00275a95ad
Алексей <alexeybudai@gmail.com>
2022-07-24 23:12:37 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -9,8 +9,8 @@ const cacheName = workbox.core.cacheNames.precache
 const setCacheName = workbox.core.setCacheNameDetails
 
 
-const {PrecacheController}=workbox.precaching
-const precacheController=new PrecacheController()
+const {PrecacheController} = workbox.precaching
+const precacheController = new PrecacheController()
 
 precacheController.addToCacheList([
     {url: '/auth/login', revision: null},
@@ -26,4 +26,57 @@ setCacheName({
 });
 workbox.core.clientsClaim();
 
-workbox.precaching.precacheAndRoute([{"revision":"99dbb1ac10f49b8b1fcc4879f60b7313","url":"favicon.ico"},{"revision":"304a1136f608847a68c957e5cda455de","url":"index.html"},{"revision":"6fd885036a48c65dc1c9476777ddfc83","url":"static/css/main.accd9ff3.css"},{"revision":"3445cd289236155e359f4ec9d70c70d4","url":"static/js/main.3121abda.js"},{"revision":"7f07a05a6495c02081ce296965f22beb","url":"asset-manifest.json"},{"revision":"b578821088f639079d5dd8764b0ca38e","url":"static/media/Calendar.10a98c21b62903a00d416f25939ddeae.svg"},{"revision":"f8fe944931626bc821b23085075f0cc9","url":"static/media/email.cfd7fc9e9b6a564041920dd6a8418ed8.svg"},{"revision":"fb91e587caa3bd4206dbce278d766110","url":"static/media/gift.6524bbfefbfc6ecc77b94cf52dd23d7d.svg"},{"revision":"9d895d740e1ca4000cb70b3d657752e5","url":"static/media/logo.80e3b7f2aa8f0e234e644ab3f5e86b66.svg"},{"revision":"65f28d384b705542ed8d0179b9fc40b7","url":"static/media/Phone.7f57311fdf0e51e1c3a76d81166f79c4.svg"},{"revision":"dc62ffcc81ee5c0cea710c09027204fd","url":"static/media/profile.d8beab90a1c3af72e650dfbb71a80de6.svg"},{"revision":"6d33824ebe9a9b702096ff730f0cbd6f","url":"static/media/qr.d90ce238c55f0df5833d9ed78d541502.svg"},{"revision":"cf2ff7c1a111bf12ea0e0565bc450c5f","url":"static/media/user.6056b4037e72f4521dc8e1fefa10472d.svg"},{"revision":"fdd15f825a6db43442a758e2db168314","url":"static/media/Geometria-Bold.3a9ed34be5c5b3d8fc3c.woff"},{"revision":"45656b73e1a07f979bfb56559b359b07","url":"static/media/Geometria-Medium.ce81051a1ed94e89b4e1.woff"},{"revision":"3c6a7d3ae76a5aec78aac69db1af15cf","url":"static/media/Geometria.b7f74b260291c82461d1.woff"},{"revision":"cd42d299828c8762ff81eb5f1fe73dd2","url":"offline.html"}])
+workbox.precaching.precacheAndRoute([{
+    "revision": "99dbb1ac10f49b8b1fcc4879f60b7313",
+    "url": "favicon.ico"
+}, {
+    "revision": "304a1136f608847a68c957e5cda455de",
+    "url": "index.html"
+}, {
+    "revision": "6fd885036a48c65dc1c9476777ddfc83",
+    "url": "static/css/main.accd9ff3.css"
+}, {
+    "revision": "3445cd289236155e359f4ec9d70c70d4",
+    "url": "static/js/main.3121abda.js"
+}, {
+    "revision": "76336b16a5abf1901468be6014389175",
+    "url": "manifest.json"
+}, {
+    "revision": "b578821088f639079d5dd8764b0ca38e",
+    "url": "static/media/Calendar.10a98c21b62903a00d416f25939ddeae.svg"
+}, {
+    "revision": "f8fe944931626bc821b23085075f0cc9",
+    "url": "static/media/email.cfd7fc9e9b6a564041920dd6a8418ed8.svg"
+}, {
+    "revision": "fb91e587caa3bd4206dbce278d766110",
+    "url": "static/media/gift.6524bbfefbfc6ecc77b94cf52dd23d7d.svg"
+}, {
+    "revision": "9d895d740e1ca4000cb70b3d657752e5",
+    "url": "static/media/logo.80e3b7f2aa8f0e234e644ab3f5e86b66.svg"
+}, {
+    "revision": "65f28d384b705542ed8d0179b9fc40b7",
+    "url": "static/media/Phone.7f57311fdf0e51e1c3a76d81166f79c4.svg"
+}, {
+    "revision": "dc62ffcc81ee5c0cea710c09027204fd",
+    "url": "static/media/profile.d8beab90a1c3af72e650dfbb71a80de6.svg"
+}, {
+    "revision": "6d33824ebe9a9b702096ff730f0cbd6f",
+    "url": "static/media/qr.d90ce238c55f0df5833d9ed78d541502.svg"
+}, {
+    "revision": "cf2ff7c1a111bf12ea0e0565bc450c5f",
+    "url": "static/media/user.6056b4037e72f4521dc8e1fefa10472d.svg"
+}, {
+    "revision": "fdd15f825a6db43442a758e2db168314",
+    "url": "static/media/Geometria-Bold.3a9ed34be5c5b3d8fc3c.woff"
+}, {
+    "revision": "45656b73e1a07f979bfb56559b359b07",
+    "url": "static/media/Geometria-Medium.ce81051a1ed94e89b4e1.woff"
+}, {"revision": "3c6a7d3ae76a5aec78aac69db1af15cf", "url": "static/media/Geometria.b7f74b260291c82461d1.woff"},
+    {
+        "revision": null,
+        "url": "auth/authMe"
+    }, {
+        "revision": null,
+        "url": "/auth/login"
+    },
+])
